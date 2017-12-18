98e2af964cf6976b66594b9466a2c38d3afa9aeb
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2017-12-18 23:15:55 +1100

    Added initial hello world page

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+	module: {
+		loaders: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['react']
+          }
+        }
+			}
+		]
+	},
+};
