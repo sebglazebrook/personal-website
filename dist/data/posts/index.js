e5444286f2f336e4de793d2b9d5f49adbaea2af7
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2017-12-29 22:43:21 +1100

    Automated (in a hacky way) updating the index html to have the lasted blogroll data

--- dist/data/posts/index.js
+++ dist/data/posts/index.js
@@ -1,10 +1,1 @@
-{
-  "posts": [
-    {
-      "header": "hello world",
-      "content_snippet": "hello from my world!",
-      "url": "/posts/1514546432-hello_world",
-      "data_url": "data/posts/1514546432-hello_world.json"
-    }
-  ]
-}
+{"posts":[{"header":"hello world","content_snippet":"hello from my world!","url":"/posts/1514547767-hello_world","data_url":"data/posts/1514547767-hello_world.json"}]}
