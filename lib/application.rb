52ce3432f42b43cbae9a2031abb18c56ba9437e9
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2017-12-30 12:02:35 +1100

    Using ruby's inbuilt logger

--- lib/application.rb
+++ lib/application.rb
@@ -2,6 +2,5 @@ require "pathname"
 PROJECT_DIR = Pathname.new(File.expand_path(File.join("..", ".."), __FILE__))
 $LOAD_PATH.unshift(PROJECT_DIR.join("lib"))
 
-def log(message) # TODO move to helper class/file
-  puts "#{Time.now}: #{message}"
-end
+require "logger"
+LOGGER = Logger.new(STDOUT)
